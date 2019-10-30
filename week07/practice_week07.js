0df3bb55bfc5c0314041ff3ef8189055eb6d7dc7
lim <96x60812@naver.com>
2019-10-30 18:59:26 +0900

    loop in js

--- week07/practice_week07.js
+++ week07/practice_week07.js
@@ -4,3 +4,11 @@ let variable1 =2.8;
 let variable2 =0.2;
 
 alert(message.concat)
+
+let s1 = "it's a-me, mario!";
+let s2 = "";
+for (let i=0; i<s1.length; i++){
+  s2 += s1[i]+s1[i];
+}
+
+alert(s2)
