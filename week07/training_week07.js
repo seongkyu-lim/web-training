0fa8fac9684d2e221661462dabb586d20d19b17d
lim <96x60812@naver.com>
2019-10-31 15:39:40 +0900

    training last commit (not completed..)

---
+++ week07/training_week07.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("btn");
+button.addEventListener("RESET",functionName);
+let spanVariable = document.getElementById("message");
+
+
+
+function functionName(){
+  spanVariable.innerHTML="Do you want to Reset?";
+}
